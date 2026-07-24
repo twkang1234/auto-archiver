c6d0a03cd3a82761c8d331a400697f8ca9045fa0
twkang1234 <ezgocpu@gmail.com>
2026-07-24 08:12:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,12 @@ export default defineConfig(() => {
     },
   };
 });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/auto-archiver/', // 👈 請補上這一行（前後都要有斜線 /，最後加逗號）
+})
